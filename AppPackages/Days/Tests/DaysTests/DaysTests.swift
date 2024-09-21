1d71f329abf5554155f91aa74ce305ef870d89e8
HOT COFFEE <danger@cctplus.dev>
2024-09-21 22:24:02 +0000

    chore: :wrench: Add missing copyright headers to 6 files

--- AppPackages/Days/Tests/DaysTests/DaysTests.swift
+++ AppPackages/Days/Tests/DaysTests/DaysTests.swift
@@ -1,3 +1,9 @@
+// Copyright 2024 CCT Plus LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
 import Testing
 
 @testable import Days
