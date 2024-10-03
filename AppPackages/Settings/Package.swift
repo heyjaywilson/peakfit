a36369c255ed5db78fee4f83ddffb135ba303564
Samuel Sainz <nsamusan@amazon.com>
2024-10-02 23:33:10 -0300

    feat: :sparkles: Create a Settings view accessible from the TabBar. New Settings package created

--- AppPackages/Settings/Package.swift
+++ AppPackages/Settings/Package.swift
@@ -4,40 +4,22 @@
 import PackageDescription
 
 let package = Package(
-	name: "AppEntry",
-	platforms: [.iOS(.v18)],
+	name: "Settings",
+	platforms: [.iOS(.v18), .macOS(.v15)],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
 		.library(
-			name: "AppEntry",
-			targets: ["AppEntry"]
-		)
-	],
-	dependencies: [
-		.package(name: "Exercises", path: "./../Exercises"),
-		.package(name: "Days", path: "./../Days"),
-		.package(name: "Settings", path: "./../Settings"),
+			name: "Settings",
+			targets: ["Settings"])
 	],
 	targets: [
 		// Targets are the basic building blocks of a package, defining a module or a test suite.
 		// Targets can depend on other targets in this package and products from dependencies.
 		.target(
-			name: "AppEntry",
-			dependencies: [
-				.byName(
-					name: "Exercises"
-				),
-				.byName(
-					name: "Days"
-				),
-				.byName(
-					name: "Settings"
-				),
-			]
-		),
+			name: "Settings"),
 		.testTarget(
-			name: "AppEntryTests",
-			dependencies: ["AppEntry"]
+			name: "SettingsTests",
+			dependencies: ["Settings"]
 		),
 	]
 )
--- AppPackages/Settings/Sources/Settings/SettingsView.swift
+++ AppPackages/Settings/Sources/Settings/SettingsView.swift
@@ -1,35 +1,29 @@
+//
+// -----------------------------------------------------------
 // Copyright 2024 CCT Plus LLC
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may obtain a copy of the License at
 //     http://www.apache.org/licenses/LICENSE-2.0
 // -----------------------------------------------------------
-// Project: AppEntry
-// Created on 9/11/24
+// Project: Settings
+// Created on 2/10/24 by Samuel Sainz
 // -----------------------------------------------------------
+//
 
-import Days
-import Exercises
-import Settings
 import SwiftUI
 
-public struct AppEntry: View {
+public struct SettingsView: View {
+
 	public init() {}
 
 	public var body: some View {
-		TabView {
-			Tab("Sets", systemImage: "dumbbell") {
-				ExerciseListView()
-			}
-			Tab("Days", systemImage: "calendar") {
-				DaysView()
-			}
-			Tab("Settings", systemImage: "gear") {
-				SettingsView()
-			}
+		NavigationStack {
+			Text("This is the Settings View")
+				.navigationTitle("Settings")
 		}
 	}
 }
 
 #Preview {
-	AppEntry()
+	SettingsView()
 }
